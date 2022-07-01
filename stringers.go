1408536e73b2c3ba53a18bf0335b15ca2eaa9bb8
Tommy <92980386+monkemon@users.noreply.github.com>
2022-07-01 13:54:42 +0200

    Update stringers.go

--- stringers.go
+++ stringers.go
@@ -1,6 +1,15 @@
 // very punk solution to a stringer excercise from the tour of go
 // https://go.dev/tour/methods/18
 
+/*
+Make the IPAddr type implement fmt.Stringer to print the address as a dotted quad.
+
+For instance, IPAddr{1, 2, 3, 4} should print as "1.2.3.4".
+*/
+
+
+
+
 func (ip IPAddr) String() string {
 	out:= fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
 	return out
